e68b214f1e9c98167abc07da1dd58fafdde75753
eduard1902 <54848296+eduard1902@users.noreply.github.com>
2020-06-04 23:10:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+let money = 6;
+let income = 10;
+let addExpenses = 'hi';
+let deposit = 9;
+let mission = 'mission';
+let period = 0;
+
+console.log(addExpenses);
+
+alert ('Hello, world!');
+
+console.log(period);
